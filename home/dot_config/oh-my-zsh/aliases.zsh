96e74a71209b53b2a4871201d510fd0c90937ecf
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-07-13 14:54:19 -0600

    feat: update aliases.zsh aliases
    - Add decompress (un*) alises
    - Update date aliases to make more logical sense, avoid node dependency
    - Rename localip to privateip
    - Add publicip alias for ip
    - Add perm alias to get permissions in octal notation in addition to ls -l
    - Add calc alias for bc

--- home/dot_config/oh-my-zsh/aliases.zsh
+++ home/dot_config/oh-my-zsh/aliases.zsh
@@ -4,15 +4,17 @@ alias \
   7z="7zz" \
   brewup="brew update && brew upgrade" \
   cafe="caffeinate" \
+  calc="bc"\
   cls="clear" \
   dockerprune="docker system prune --all --force --volumes" \
   exif="exiftool" \
   git-stash-changed="git stash push --keep-index --include-untracked" \
-  localip="ipconfig getifaddr en0" \
   mimetype="file --brief --mime-type" \
   mkdirp="mkdir -p" \
   ncp="corepack" \
+  perm="stat -f '%Lp %Sp %Su %Sg %N'" \
   pn="pnpm" \
+  privateip="ipconfig getifaddr en0" \
   resudo="sudo !!" \
   rmrf="rm -f -R --" \
   symlink="ln -s" \
@@ -21,27 +23,36 @@ alias \
   unrebase="rm -rf ./.git/rebase-merge" \
   unquarantine="xattr -d com.apple.quarantine" \
   wh="builtin where" \
-  whereami="builtin pwd " \
+  whereami="builtin pwd" \
   whisper-cli="whisper-cli --model $XDG_DATA_HOME/whisper/ggml-small.bin" \
   yt="yt-dlp" \
   yt-mp4="yt-dlp --remux-video mp4" \
   yt-mp3="yt-dlp --format 'bestaudio*' --no-write-subs --extract-audio --audio-format mp3 --audio-quality 0 --no-embed-chapters"
+
+# To match uncompress, unxz, unzip, unzstd
+alias \
+  unbzip2="bunzip2" \
+  ungz="gunzip" \
+  untar="tar --extract --file"
 
 # Get public IP address. Website created by the wonderful Major Hayden and is now
 # managed by Cloudflare: https://major.io/p/a-new-future-for-icanhazip/
 alias \
   ip="curl --silent --location --fail --show-error https://icanhazip.com" \
   ipv4="curl --silent --location --fail --show-error https://ipv4.icanhazip.com" \
-  ipv6="curl --silent --location --fail --show-error https://ipv6.icanhazip.com"
+  ipv6="curl --silent --location --fail --show-error https://ipv6.icanhazip.com" \
+  publicip="ip"
 
 # `now` uses `date` with specific format to get current instant as ISO 8601 date
-# since `date -Iseconds` is not technically correct (+00:00 instead of Z)
+# in UTC timezone since `date -Iseconds` provides timezone offset
 alias \
-  now="date -u +%Y-%m-%dT%H:%M:%SZ" \
+  now="date -u +%FT%TZ" \
   nowtz="date -Iseconds" \
+  iso8601="now" \
   rfc2822="date -R" \
   timestamp="date +%s" \
-  timestampms="FORCE_COLOR=0 node --print 'Date.now()'"
+  timestampms="expr $(date +%s%N) / 1000000" \
+  timestampns="date +%s%N"
 
 # These are technically longer but it's easier to remember. Note that if no
 # files are passed to `wc`, it will read from stdin.
